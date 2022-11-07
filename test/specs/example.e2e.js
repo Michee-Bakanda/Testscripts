3c2a804f5142b7eefe675aa2d06f04ed35b1cb69
michee <micbakanda@gmail.com>
2022-11-07 13:37:04 +0200

    testscrtipt

---
+++ test/specs/example.e2e.js
@@ -0,0 +1,17 @@
+// npx wdio wdio.conf.js
+
+const LoginPage = require('../pageobjects/login.page');
+const SecurePage = require('../pageobjects/secure.page');
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open();
+
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        await expect(SecurePage.flashAlert).toBeExisting();
+        await expect(SecurePage.flashAlert).toHaveTextContaining(
+            'You logged into a secure area!');
+    });
+});
+
+
